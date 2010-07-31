3f4f81af8d30852c7fb92feb5264948b1c2e80d5
Oscar Koeroo <okoeroo@gmail.com>
2010-08-01 00:07:03 +0200

    Adding intermediate BeautifulSoup-ness

--- indico_xml_to_hope_json/indico_xml_to_hope_json.py
+++ indico_xml_to_hope_json/indico_xml_to_hope_json.py
@@ -25,12 +25,11 @@ def wget(url):
 
 
 def pull_conference(url):
-    page = wget(url)
+    #page = wget(url)
+    page = open('data.html').read()
 
     doc = BeautifulSoup(page, convertEntities=BeautifulSoup.HTML_ENTITIES)
     if doc:
-        # print doc
-
         iconf = doc.findAll('pre')
         my_iconf = BeautifulStoneSoup(iconf[0].string)
 
@@ -42,11 +41,48 @@ conference = pull_conference(indico_url)
 
 
 for session in conference.findAll('session'):
-    print "*----"
-    #print session
-    print session.id
-    print session.id.contents
-    print session.id.contents[0]
+    print "*-------------------------------------------------------------"
+    print session
+    print "ID:         " + session.id.contents[0]
+    print "code:       " + session.code.contents[0]
+    print "Title       " + session.title.contents[0]
+    if session.convener:
+        print "Convener:   " + session.convener.contents[0]
+
+    print "*---------------------"
+    print session.convener
+    print "*---------------------"
+
+
+#    if session.user and session.user.title:
+#        print "User title: " + session.user.title[0]
+    if session.user:
+        if session.user.title.string:
+            print "Title        :  " + session.user.title.string
+        if session.user.name:
+            print "Name         :  " + session.user.name
+        if session.user.organization.string:
+            print "Organization :  " + session.user.organization.string
+        if session.user.email.string:
+            print "Email        :  " + session.user.email.string
+#    if session.user and session.user.email:
+    print session.user
+
+#    if session.user and session.user.email:
+#        print "User email: " + session.user.email[0]
+    #print session.location
+    #print "Location name: " + session.location.name.string
+    #print session.location.room
+    #print "Location addr: " + session.location.address.string
+    print "Location room: " + session.location.room.string
+    print "Start time   : " + session.startdate.string
+    print "Stop time    : " + session.enddate.string
+    if session.description.string:
+        print "Description  : " + session.description.string
+
+
+#    print "**----"
+
 
     #for i in session:
         #print "**---"
