d93ad092da148f386ea26536d13e97be64d0a11a
Oscar Koeroo <okoeroo@gmail.com>
2010-08-02 12:16:53 +0200

    Most recent hacks

--- indico_xml_to_hope_json/indico_xml_to_hope_json.py
+++ indico_xml_to_hope_json/indico_xml_to_hope_json.py
@@ -42,50 +42,86 @@ conference = pull_conference(indico_url)
 
 for session in conference.findAll('session'):
     print "*-------------------------------------------------------------"
-    print session
-    print "ID:         " + session.id.contents[0]
-    print "code:       " + session.code.contents[0]
-    print "Title       " + session.title.contents[0]
+    #print session
+    print "ID                     : " + session.id.contents[0]
+    print "code                   : " + session.code.contents[0]
+    print "Title                  : " + session.title.contents[0]
+
     if session.convener:
-        print "Convener:   " + session.convener.contents[0]
+#        print "len(session.convener) " + str(len(session.convener))
+        for user_tag in session.convener.findAll('user'):
+            print user_tag.__class__
+            print user_tag.__class__.__name__
+            for user_tag_content in user_tag:
+                print "**---"
+                print user_tag_content.__class__
+                print user_tag_content.__class__.__name__
+                print user_tag_content
+                print "**---"
+
+            # print users.string
+
+#        print session.convener.__class__
+#        for conv in session.convener:
+#            print conv.__class__
+#            print "len(conv) " + str(len(conv))
+#            for c in conv:
+#                print c.__class__
+#                print "foo"
+#                for i in c:
+#                    print i
+
+
+        #print len(session.convener.user)
+
+    if session.convener and session.convener.user:
+        if session.convener.user.title and session.convener.user.title.string:
+            print "Convener title         : " + session.convener.user.title.string
+        if session.convener.user.organization and session.convener.user.organization.string:
+            print "Convener organization  : " + session.convener.user.organization.string
+        if session.convener.user.email and session.convener.user.email.string:
+            print "Convener email         : " + session.convener.user.email.contents[0]
+
+        #for userinfo in session.convener.user.contents:
+        #    print "---"
+        #    print userinfo
+
+        #    for u in userinfo:
+        #        print u
+
+            #if userinfo and userinfo.string:
+            #    print "Convener some user     : " + str(userinfo) + " " + userinfo.string
+#            print userinfo.organization
+        #    print "---"
+
+
+
+    if session.location:
+        if session.location.room and session.location.room.string:
+            print "Location room          : " + session.location.room.string
+        if session.location.address and session.location.address.string:
+            print "Location addr          : " + session.location.address.string
+
+    print "Start time             : " + session.startdate.string
+    print "Stop time              : " + session.enddate.string
+    #if session.description.string:
+    #    print "Description  : " + session.description.string
+
 
     print "*---------------------"
     print session.convener
+    print "*--"
+    if session.convener and session.convener.user:
+        print session.convener.user
+    print "*--"
+    print session.location
     print "*---------------------"
 
 
-#    if session.user and session.user.title:
-#        print "User title: " + session.user.title[0]
-    if session.user:
-        if session.user.title.string:
-            print "Title        :  " + session.user.title.string
-        if session.user.name:
-            print "Name         :  " + session.user.name
-        if session.user.organization.string:
-            print "Organization :  " + session.user.organization.string
-        if session.user.email.string:
-            print "Email        :  " + session.user.email.string
-#    if session.user and session.user.email:
-    print session.user
 
-#    if session.user and session.user.email:
-#        print "User email: " + session.user.email[0]
     #print session.location
     #print "Location name: " + session.location.name.string
     #print session.location.room
     #print "Location addr: " + session.location.address.string
-    print "Location room: " + session.location.room.string
-    print "Start time   : " + session.startdate.string
-    print "Stop time    : " + session.enddate.string
-    if session.description.string:
-        print "Description  : " + session.description.string
-
-
-#    print "**----"
-
-
-    #for i in session:
-        #print "**---"
-        #print i
 
 
