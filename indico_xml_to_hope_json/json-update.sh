ffae3760868ac30212fe0c3002522fca1d277660
Oscar Koeroo <okoeroo@gmail.com>
2010-09-11 13:47:36 +0200

    Adding publishing script

---
+++ indico_xml_to_hope_json/json-update.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+JSON="data.json"
+ENTERHOST="login2.nikhef.nl"
+PUBLISH_DIR="/www/tmpfiles/egi2010/"
+BACKUPDIR="${PUBLISH_DIR}/backup"
+
+
+function backupfailed() {
+    echo "Backup failed on remote service"
+    exit 1
+}
+function publishingfailed() {
+    echo "Publishing the ${JSON} file at ${ENTERHOST}:${PUBLISH_DIR}/${JSON} failed"
+    exit 1
+}
+
+
+ssh ${ENTERHOST} cp ${PUBLISH_DIR}/${JSON} ${BACKUPDIR}/data.json.`date +%Y%m%d%H%M` || backupfailed
+
+python indico_xml_to_hope_json.py || exit 1
+
+scp ${JSON} ${ENTERHOST}:${PUBLISH_DIR}/${JSON} || publishingfailed
